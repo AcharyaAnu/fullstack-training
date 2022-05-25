95ddd9df16e8dca3f56e09768a91d3066287a563
AcharyaAnu <104482517+AcharyaAnu@users.noreply.github.com>
2022-05-25 10:09:41 +0530

    Add files via upload
    Yelp Camp

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const figlet=require("figlet");
-const colors=require("colors")
-figlet ("helo world",function(err,data){
-    if(err){
-        console.log("something went wrong");
-        console.dir(err);
-        return;
+// const express=require('express');
+// const path=require('path');
+const mongoose=require('mongoose');
+const Campground=require('../models/campground');
+const cities=require('./cities');
+const {places,description, descriptors}=require('./seedHelpers')
 
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// })
+
+const db=mongoose.connection;
+db.on('error',console.error.bind(console,'connection error'));
+db.once('open',()=>{
+    console.log("database connected");
+});
+const sample= array => array[Math.floor(Math.random()*array.length)];
+
+const seedDB=async () =>{
+    await Campground.deleteMany({});
+    for(let i=0; i<50; i++){
+        const random1000=Math.floor(Math.random()*1000);
+        const camp=new Campground({
+            location:`${cities[random1000].city},${cities[random1000].state}`,
+            title:`${sample(descriptors)}   ${sample(places)}`
+        })
+        await camp.save();
     }
-    console.log(data.blue)
+}
+
+seedDB().then(()=>{
+    mongoose.connection.close()
 })
