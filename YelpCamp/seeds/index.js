6c5bbb317db53a75db5fa6f02d91541e7da48f58
AcharyaAnu <104482517+AcharyaAnu@users.noreply.github.com>
2022-05-25 16:27:28 +0530

    25 may-yelp camp

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -23,9 +23,13 @@ const seedDB=async () =>{
     await Campground.deleteMany({});
     for(let i=0; i<50; i++){
         const random1000=Math.floor(Math.random()*1000);
+        const price=Math.floor(Math.random()*20)+10;
         const camp=new Campground({
             location:`${cities[random1000].city},${cities[random1000].state}`,
-            title:`${sample(descriptors)}   ${sample(places)}`
+            title:`${sample(descriptors)}   ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tempora consequuntur voluptatibus, perspiciatis modi, fugiat illo non distinctio ab mollitia, alias quas. Ducimus doloribus reprehenderit ex fuga, assumenda odit natus?',
+            price
         })
         await camp.save();
     }
